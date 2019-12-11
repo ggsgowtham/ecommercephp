a5c360ed1e7f2267f4307c41a3a53aaf938dc8b9
gowtham <ggsgowtham17@gmail.com>
2019-12-11 20:09:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ecommerce Site</title>
+
+	<link rel="stylesheet" type="text/css" href="styles/style.css" media="all">
+</head>
+<body>
+	<div class="main_wrapper">
+		<!-- Header starts here -->
+		<div class="header_wrapper">
+			<img src="images/logo.jpg"  id="logo" alt="Logo" />
+			<img src="images/logo1.png" id="banner" alt="menu logo" />
+		</div>
+		<!-- Header Ends here -->
+		<br><br>
+		<!-- Navigation starts here -->
+		<div class="menubar">
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+		<!-- Navigation Ends here -->
+
+		<!-- Content starts here -->
+		<div class="content_wrapper">
+			<!-- <div>This is menubar!!</div> -->
+			<div id="sidebar">This is sidebar!!</div>
+			<div id="content_area">This is content!!</div>
+		</div>
+		<!-- Content Ends here -->
+
+		<!-- Footer starts here -->
+		<div id="footer">This is footer!!</div>
+		<!-- Footer Ends here -->
+	</div>
+</body>
+</html>
